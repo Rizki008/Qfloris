0bff6bf58f09ab00b1ba9338f4460453fbe82e37
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2022-05-13 23:37:46 +0700

    mengubah logo toko

--- application/views/backend/v_nav.php
+++ application/views/backend/v_nav.php
@@ -5,9 +5,9 @@ $total_pesanan = $this->m_admin->total_pesanan();
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
 	<!-- Brand Logo -->
 	<a href="<?= base_url('admin') ?>" class="brand-link">
-		<img src="<?= base_url() ?>assets/logo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+		<img src="<?= base_url() ?>assets/Qflorist.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
 		<span class="brand-text font-weight-light">
-			<h5>Toko Mutiara</h5>
+			<h5>Qflorist</h5>
 		</span>
 	</a>
 
