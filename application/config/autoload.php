0378c60deb4f9ec9563ddcc6ca8ca2a9b769c86a
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2022-05-14 00:31:41 +0700

    mengubah route utama dan proses model utama

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('session', 'form_validation', 'database', 'upload', 'user_login');
+$autoload['libraries'] = array('session', 'form_validation', 'database', 'upload', 'user_login', 'pelanggan_login', 'cart');
 
 /*
 | -------------------------------------------------------------------
@@ -89,7 +89,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array('file', 'url', 'form', 'string');
+$autoload['helper'] = array('file', 'url', 'global', 'form', 'string');
 
 /*
 | -------------------------------------------------------------------
@@ -132,4 +132,4 @@ $autoload['language'] = array();
 |
 |	$autoload['model'] = array('first_model' => 'first');
 */
-$autoload['model'] = array();
+$autoload['model'] = array('m_home');
